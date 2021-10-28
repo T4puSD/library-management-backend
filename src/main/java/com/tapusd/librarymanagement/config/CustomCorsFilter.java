471911b25dd61932d9c24af5cb0e6cd4b35a9408
T4puSD <sdtapusd20@gmail.com>
2021-10-29 00:41:29 +0600

    configured cors to only allow front end url

---
+++ src/main/java/com/tapusd/librarymanagement/config/CustomCorsFilter.java
@@ -0,0 +1,35 @@
+package com.tapusd.librarymanagement.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+public class CustomCorsFilter extends org.springframework.web.filter.CorsFilter {
+    @Value("${front-end.service.url}")
+    private String frontendUrl;
+
+    public CustomCorsFilter(CorsConfigurationSource configSource) {
+        super(configSource);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", frontendUrl);
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request,response);
+        }
+    }
+}
