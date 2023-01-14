55028c762691123395c2fbe19202d30fc029c49a
T4puSD <sdtapusd20@gmail.com>
2023-01-14 21:22:43 +0600

    configured account details service and removed in memory user details

--- src/main/java/com/tapusd/librarymanagement/config/SecurityConfigurer.java
+++ src/main/java/com/tapusd/librarymanagement/config/SecurityConfigurer.java
@@ -6,39 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurer {
     private static final String H2_CONSOLE_ANT_MATCHER = "/h2-console/**";
-
-    @Bean
-    public  UserDetailsService userDetailsService() {
-        UserDetails student = User.withDefaultPasswordEncoder()
-                .username("student")
-                .password("password")
-                .roles(Role.STUDENT.name())
-                .build();
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("password")
-                .roles(Role.ADMIN.name())
-                .build();
-
-        UserDetails facalty= User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("password")
-                .roles(Role.FACULTY.name())
-                .build();
-
-        return new InMemoryUserDetailsManager(student, admin, facalty);
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
---
+++ src/main/java/com/tapusd/librarymanagement/security/AccountDetailsService.java
@@ -0,0 +1,26 @@
+package com.tapusd.librarymanagement.security;
+
+import com.tapusd.librarymanagement.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountDetailsService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountDetailsService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return accountRepository.findByEmail(username)
+                .map(CustomAccountDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("No user found with provided email!"));
+    }
+}
