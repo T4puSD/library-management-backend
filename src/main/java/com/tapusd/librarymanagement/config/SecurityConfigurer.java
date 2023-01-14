ea3a9982c0a995939ae6633341cc81d03675488f
T4puSD <sdtapusd20@gmail.com>
2023-01-14 21:02:44 +0600

    updated project to spring boot 3

--- src/main/java/com/tapusd/librarymanagement/config/SecurityConfigurer.java
+++ src/main/java/com/tapusd/librarymanagement/config/SecurityConfigurer.java
@@ -1,37 +1,45 @@
 package com.tapusd.librarymanagement.config;
 
 import com.tapusd.librarymanagement.domain.enumeration.Role;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+public class SecurityConfigurer {
     private static final String H2_CONSOLE_ANT_MATCHER = "/h2-console/**";
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").roles("ADMIN").password("password");
+    @Bean
+    public  UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .headers()
-                    .frameOptions().sameOrigin()
-                .and()
-                .csrf()
-                .ignoringAntMatchers(H2_CONSOLE_ANT_MATCHER)
-                .disable()
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .headers(headerConfig -> headerConfig.frameOptions().sameOrigin())
+                .csrf(AbstractHttpConfigurer::disable)
                 .cors()
                 .and()
-                .authorizeRequests()
-                .antMatchers("/admin/**").hasRole(Role.ADMIN.name())
-                .antMatchers("/user/**").hasAnyRole(Role.ADMIN.name(), Role.STUDENT.name(), Role.FACULTY.name())
-                .antMatchers(H2_CONSOLE_ANT_MATCHER).permitAll()
-                .anyRequest().permitAll();
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
+                        .requestMatchers("/user/**").hasAnyRole(Role.ADMIN.name(),
+                                Role.STUDENT.name(), Role.FACULTY.name())
+                        .requestMatchers(H2_CONSOLE_ANT_MATCHER).permitAll()
+                        .anyRequest().permitAll())
+                .build();
     }
 }
